c74ac60bf63e7b6430dfb1b75bb65560b3b264b8
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-12-01 21:01:33 +0900

    vueページ作成

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
 
     <about-me />
     <java-script />
+    <vue-page />
   </div>
 </template>
 
@@ -11,12 +12,14 @@
 import Header from '~/components/Header.vue'
 import AboutMe from '~/components/pages/AboutMe.vue'
 import JavaScript from '~/components/pages/JavaScript.vue'
+import VuePage from '~/components/pages/VuePage.vue'
 
 export default {
   components: {
     Header,
     AboutMe,
-    JavaScript
+    JavaScript,
+    VuePage
   }
 }
 </script>
