b3a620b2134983bf81aaeee4a5d2bd8c72ea538f
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-12-02 10:36:34 +0900

    footerページ作成

---
+++ components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="">
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="copy-right col-sm-12">
+            <small
+              >&copy; 2019-<span>{{ thisYear }}</span> koki-iwahara</small
+            >
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    thisYear() {
+      const date = new Date()
+      const thisYear = date.getFullYear()
+      return thisYear
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  border-top: 1px solid rgba(0, 0, 0, 0.125);
+}
+
+footer {
+  background-color: #f4f5f7;
+  padding-bottom: 30px;
+}
+.copy-right {
+  padding-top: 30px;
+}
+</style>
